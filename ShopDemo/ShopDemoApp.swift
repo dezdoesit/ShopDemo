13ff4c4babf6be5940c0b19e06cc667b792032fe
dezdoesit <dez.blair@icloud.com>
2023-10-18 09:06:05 -0400

    Initial Commit

---
+++ ShopDemo/ShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDemoApp.swift
+//  ShopDemo
+//
+//  Created by dmoney on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopDemoUITests/ShopDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShopDemoUITestsLaunchTests.swift
+//  ShopDemoUITests
+//
+//  Created by dmoney on 10/18/23.
+//
+
+import XCTest
+
+final class ShopDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
